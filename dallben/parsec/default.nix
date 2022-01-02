99d8d97570f3a7dabe47b6f4ff0c83c780aec555
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-01-02 02:28:17 -0800

    Get rid of `warning: user ‘gurgi’ has unknown group ‘gurgi’`

--- dallben/parsec/default.nix
+++ dallben/parsec/default.nix
@@ -10,7 +10,6 @@
   # TODO: lock down permissions so that's the *only* thing it can do.
   users.users.gurgi = {
     isNormalUser = true;
-    group = "gurgi";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPog+FoId+C37SnL1VfwRE11pGzzvxOM0GL0HjOL1Qqf gurgi@snowdon"
     ];
