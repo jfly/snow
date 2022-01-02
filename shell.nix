7b5fe96c410a956687863ff2e4d1e57496c81f4e
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-01-02 00:17:27 -0800

    We don't need a customized nixpkgs here.

--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
 {
   pkgs ? import (builtins.fetchTarball {
-    name = "jfly-nixos-21.11";
-    url = "https://github.com/jfly/nixpkgs/archive/0229e39c316e882ca3884712690c8833207ea8ff.tar.gz";
+    name = "nixos-21.11";
+    url = "https://github.com/jfly/nixpkgs/archive/a7ecde854aee5c4c7cd6177f54a99d2c1ff28a31.tar.gz";
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "173d00jq26n2v9m6glglkph742l422q2vn45agh7x3aw3c23vl6a";
+    sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
   }) {}
 }:
 
