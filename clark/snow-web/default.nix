a701ba9c68af96c10e75df61a4afc8fb7a66c1cb
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-04-17 01:25:53 -0700

    More cleanup of snow-web

--- clark/snow-web/default.nix
+++ clark/snow-web/default.nix
@@ -2,37 +2,23 @@
 
 let
   nginx-conf = ./etc/nginx/nginx.conf;
-  service-snow-web-conf = ./etc/nginx/service-snow-web.conf;
+  nginx-conf-d = ./etc/nginx/conf.d;
   webroot = ./webroot;
-
-  password = pkgs.deage.string ''
-    -----BEGIN AGE ENCRYPTED FILE-----
-    YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSB4NzRNQzQ4ZVJuRGc4UWJh
-    aTdDN2RnQ2lycGNxVEY2Y3dycEhBQ3U2MGk0CjJXZHlzTHV5NDJNS1g5RUFZeHov
-    bGV2QWgxMHlJUnBJdHZhQVU0MGVncXcKLS0tIFdIeVFQOXN1QzJ3K2d3dVJyajRt
-    bUF4emdkdjFINS9aeGJ2SU9GNjArS3MKTrHgg6oK9NIL9fJC3SeK8SB9ihvAbh6/
-    4zdCCMJUPGkDEARi/HmQmQ==
-    -----END AGE ENCRYPTED FILE-----
-  '';
-  auth-root = pkgs.writeText "nginx-auth-root" "clark:{PLAIN}${password}";
 in
 
 {
-  image = "umputun/nginx-le:latest";
+  image = "nginx:latest";
   ports = [
     "8080:80"
   ];
   volumes = [
-    "/mnt/media/state/snow-web/nginx/ssl:/etc/nginx/ssl"
     "${nginx-conf}:/etc/nginx/nginx.conf"
-    "${service-snow-web-conf}:/etc/nginx/service-snow-web.conf"
-    "${auth-root}:/etc/nginx/auth/root"
+    "${nginx-conf-d}:/etc/nginx/conf.d"
     "${webroot}:/webroot"
     "/mnt/media:/mnt/media"
   ];
   environment = {
     TZ = "America/Los_Angeles";
-    LETSENCRYPT = "false";
   };
   extraOptions = [
     "--network=clark"
