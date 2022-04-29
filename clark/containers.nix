3e6e25121e767c349fcf642697c0d79edba97a31
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-04-28 20:45:02 -0700

    Add a daily backup of /state
    URG I REALLY MESSED THIS UP AT FIRST AND STARTED DELETING /. URG

--- clark/containers.nix
+++ clark/containers.nix
@@ -17,6 +17,19 @@ let
           username: k8s
           password: ${password}
   '';
+  state-backup = pkgs.writeShellScriptBin "state-backup" ''
+      export PATH=$PATH:${pkgs.findutils}/bin
+      export PATH=$PATH:${pkgs.gnutar}/bin
+
+      # Create a backup with today's date
+      backup=/mnt/media/backups/clark-state-$(date -I).tar
+      wip_backup=$backup.wip
+      tar cfp "$wip_backup" /state
+      mv "$wip_backup" "$backup"
+
+      # Remove backups more than 10 days old
+      find /mnt/media/backups -type f -mtime +10 -delete
+  '';
 in
 {
   # Set up a kubernetes cluser with k3s
@@ -42,4 +55,23 @@ spec:
       '';
   };
   environment.systemPackages = [ pkgs.k3s ];
+
+  # Do a daily backup of /state
+  # TODO: also do mysqldump/pg_dump to deal with the fact that this is not
+  # atomic and databases are probably finnicky
+  systemd = {
+    timers.state-backup = {
+      description = "/state backup timer";
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "daily";
+        Unit = "state-backup.service";
+      };
+    };
+    services.state-backup = {
+      description = "/state backup";
+      enable = true;
+      script = "${state-backup}/bin/state-backup";
+    };
+  };
 }
