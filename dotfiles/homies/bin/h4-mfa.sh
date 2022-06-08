84958b30de336b4ef013a3032196a509d38fb024
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-06-08 09:49:17 -0700

    Add support for pulling mfa from phone

--- dotfiles/homies/bin/h4-mfa.sh
+++ dotfiles/homies/bin/h4-mfa.sh
@@ -4,23 +4,32 @@ set -e
 
 profile=honor-temp
 
-if aws --profile="$profile" iam get-user > /dev/null; then
+if aws --profile="$profile" iam get-user >/dev/null; then
     # The temp credentials are still working! Nothing to do here.
     exit 0
 fi
 
-# Note we're careful to only print to /dev/stderr. That's because this script
-# might be called via a clever ssh ProxyCommand, and printing to stdout would
-# actually inject bytes into the SSH TCP connection!
-echo "" > /dev/stderr
-echo "Your credentials for AWS profile $profile appear to be out of date. Refreshing..." > /dev/stderr
-echo "Please enter a MFA code in the dialog that appears." > /dev/stderr
-# We can't read from stdin here because it would mess up the ssh protocol (see
-# comment above about /dev/stderr).
-MFA=$(zenity --title="Enter MFA code" --text="Enter MFA code" --entry)
+MFA=""
+# First, try to get the 2fa from an attached cell phone.
+if two_factors=$(2fa-cli 2>&1); then
+    MFA=$(echo "$two_factors" | grep "Amazon Web Services" | cut -f 1)
+fi
+
+# If we couldn't get a MFA from an attached phone, just prompt the user.
+if [ -z "$MFA" ]; then
+    # Note we're careful to only print to /dev/stderr. That's because this script
+    # might be called via a clever ssh ProxyCommand, and printing to stdout would
+    # actually inject bytes into the SSH TCP connection!
+    echo "" >/dev/stderr
+    echo "Your credentials for AWS profile $profile appear to be out of date. Refreshing..." >/dev/stderr
+    echo "Please enter a MFA code in the dialog that appears." >/dev/stderr
+    # We can't read from stdin here because it would mess up the ssh protocol (see
+    # comment above about /dev/stderr).
+    MFA=$(zenity --title="Enter MFA code" --text="Enter MFA code" --entry)
+fi
 
 if [ -z "$MFA" ]; then
-    echo "You must enter a MFA" > /dev/stderr
+    echo "You must enter a MFA" >/dev/stderr
     exit 1
 fi
 
@@ -29,8 +38,8 @@ ACCESS_KEY=$(echo "$TOKEN" | jq -r '.Credentials.AccessKeyId')
 SECRET_KEY=$(echo "$TOKEN" | jq -r '.Credentials.SecretAccessKey')
 SESSION=$(echo "$TOKEN" | jq -r '.Credentials.SessionToken')
 
-echo "=> updating ~/.aws/credentials as profile $profile" > /dev/stderr
+echo "=> updating ~/.aws/credentials as profile $profile" >/dev/stderr
 aws configure set --profile "$profile" aws_access_key_id "$ACCESS_KEY"
 aws configure set --profile "$profile" aws_secret_access_key "$SECRET_KEY"
 aws configure set --profile "$profile" aws_session_token "$SESSION"
-echo "[OK] done" > /dev/stderr
+echo "[OK] done" >/dev/stderr
