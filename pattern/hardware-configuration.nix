57ae4d9b82a73051d7e850c92021a4436f9ac647
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-08-11 00:23:21 -0700

    Turn on some system76 daemons
    Honestly not sure what these do yet

--- pattern/hardware-configuration.nix
+++ pattern/hardware-configuration.nix
@@ -32,4 +32,5 @@
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.system76.enableAll = true;
 }
