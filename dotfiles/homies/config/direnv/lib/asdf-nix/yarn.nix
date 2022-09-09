b12e341843b89931bd6dabefee671ad40c3b2a61
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-09-08 22:56:43 -0700

    Tell yarn to use a "system" install of node
    See comment in the diff for why.

--- dotfiles/homies/config/direnv/lib/asdf-nix/yarn.nix
+++ dotfiles/homies/config/direnv/lib/asdf-nix/yarn.nix
@@ -6,7 +6,16 @@ let
     "1.22.17" = "sha256-cvsgEPlh4B/jEsLQboO2xMslhSAkuWFAoMucJLanf+o=";
   };
 in
-version: pkgs.yarn.overrideAttrs (oldAttrs: {
+version: (pkgs.yarn.override {
+  # This is sort of strange: yarn really wants to run using whatever version
+  # of nodejs you're actually using in your project. At the very least, its
+  # "check engines" feature looks at the version of node you're using to run
+  # *yarn*, as opposed to whatever the `node` binary happens to point at. :shrug:
+  # Fortunately, nix's yarn package can be configured to use a "system"
+  # install of node by just nulling out nodejs. Sweet!
+  # Trick copied from https://github.com/NixOS/nixpkgs/issues/53820#issuecomment-617973476
+  nodejs = null;
+}).overrideAttrs (oldAttrs: {
   version = version;
   src = fetchzip {
     url = "https://github.com/yarnpkg/yarn/releases/download/v${version}/yarn-v${version}.tar.gz";
