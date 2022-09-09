b4617333b8976c68aa99103f3982f5264ca51706
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-09-09 03:32:21 -0700

    Purify asdf-nix nodejs plugin

--- dotfiles/homies/config/direnv/lib/asdf-nix/nodejs.nix
+++ dotfiles/homies/config/direnv/lib/asdf-nix/nodejs.nix
@@ -1,15 +1,15 @@
 { pkgs, callPackage }:
 
 let
-  buildNodejs = callPackage (<nixpkgs> + "/pkgs/development/web/nodejs/nodejs.nix") {
-    python = pkgs.python3;
-  };
   shaByVersion = {
     "14.18.2" = "sha256-Poqc4Q+LzTYo623QSffwPIS6khm+b5dD4iIRVLnMaAs=";
   };
 in
-version: buildNodejs {
-  enableNpm = true;
-  version = version;
-  sha256 = shaByVersion.${version};
-}
+version: pkgs.nodejs.overrideAttrs (oldAttrs: {
+  inherit version;
+  src = pkgs.fetchurl {
+    url = "https://nodejs.org/dist/v${version}/node-v${version}.tar.xz";
+    sha256 = shaByVersion.${version};
+  };
+  patches = [ ];
+})
