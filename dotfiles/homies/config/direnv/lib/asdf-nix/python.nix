97f47b32cfc533553d8dd23208f7a70fc1c51623
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-09-09 19:00:00 -0700

    Add python2 to asdf-nix
    THIS WAS AN INSANE AMOUNT OF WORK. I had to use pip2nix to get a
    the right set of pip dependencies to install virtualenv, and after I
    spent an eternity on that, it turns out that virtualenv doesn't even
    work with nix's python2 setup. Thankfully, there's a patch (linked to
    below) that address *that* issue.

--- dotfiles/homies/config/direnv/lib/asdf-nix/python.nix
+++ dotfiles/homies/config/direnv/lib/asdf-nix/python.nix
@@ -10,6 +10,16 @@ let
       };
       sha256 = "sha256-avJKZgk92EC8zPNx1ARKMCfmVc8kWRzibkgCK8eSGdk=";
     };
+    "2.7.17" = (pkgs.python27.override {
+      sourceVersion = {
+        major = "2";
+        minor = "7";
+        patch = "17";
+        suffix = "";
+      };
+      sha256 = "sha256-TUPwM829Cqe3AjyBsOmG/RHmU7UkjayRRNUI8RgSukE=";
+      packageOverrides = pkgs.callPackage ./py2-virtualenv-packages.nix { };
+    }).withPackages (ps: with ps; [ pip virtualenv ]);
   };
 in
 version: versions.${version}
