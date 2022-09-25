8537e27e2886b6068b41bcfccaa1f2148d289301
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-09-25 01:05:23 -0700

    Increase gpg-agent timeout
    The defaults (10 minutes + 2 hours) are too restrictive for my taste. It
    basically means I have to enter my passphrase every time I write a
    commit message.
    
    12 hours feels pretty reasonable to me: I expect I'll only have to enter
       my passphrase once a day.

--- pattern/development.nix
+++ pattern/development.nix
@@ -1,6 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-let nm-vpn-add = pkgs.callPackage ../shared/nm-vpn-add { };
+let
+  nm-vpn-add = pkgs.callPackage ../shared/nm-vpn-add { };
+  # Reconfigure gpg-agent to have a longer lived cache: up to 8 hours after
+  # last used, but the cache also expires when it is 8 hours old, even if it
+  # has been used recently.
+  gpg-agent_conf = pkgs.writeTextFile {
+    name = "gpg-agent.conf";
+    text = ''
+      default-cache-ttl ${toString (12 * 3600)}
+      max-cache-ttl ${toString (12 * 3600)}
+    '';
+  };
 in
 {
   # Enable docker for the main user.
@@ -19,6 +30,19 @@ in
 
   # Enable gpg agent
   programs.gnupg.agent.enable = true;
+  systemd.user.services.gpg-agent =
+    let cfg = config.programs.gnupg;
+    in
+    {
+      serviceConfig.ExecStart = [
+        ""
+        ''
+          ${cfg.package}/bin/gpg-agent --supervised \
+            --pinentry-program ${pkgs.pinentry.${cfg.agent.pinentryFlavor}}/bin/pinentry \
+            --options ${gpg-agent_conf}
+        ''
+      ];
+    };
 
   environment.systemPackages = with pkgs; [
     ### Network
