9ceca321d2f72ffacbfa256ca936227fc3c78ffb
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-10-01 00:32:46 -0700

    Add support for py 3.6.9

--- dotfiles/homies/config/direnv/lib/asdf-nix/python.nix
+++ dotfiles/homies/config/direnv/lib/asdf-nix/python.nix
@@ -10,6 +10,15 @@ let
       };
       sha256 = "sha256-avJKZgk92EC8zPNx1ARKMCfmVc8kWRzibkgCK8eSGdk=";
     };
+    "3.6.9" = pkgs.python37.override {
+      sourceVersion = {
+        major = "3";
+        minor = "6";
+        patch = "9";
+        suffix = "";
+      };
+      sha256 = "sha256-Xi9fVU4/j38ClvfnPYYAxOmsuu5rJVW4Mgbt9RU4cNo=";
+    };
     "2.7.17" = (pkgs.python27.override {
       sourceVersion = {
         major = "2";
