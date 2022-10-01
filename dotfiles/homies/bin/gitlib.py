78f40cbbc0e0639459c9bcf25e7c227f298413f3
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-10-01 00:32:20 -0700

    Add support for git ssh:// style urls

--- dotfiles/homies/bin/gitlib.py
+++ dotfiles/homies/bin/gitlib.py
@@ -7,15 +7,13 @@ __all__ = ['parse_remote']
 def parse_remote(remote: str):
     regexps = [
         re.compile(r"^git@(?P<service>[^/]+):(?P<org>.+)/(?P<repo>[^/]+).git$"),
-        re.compile(r"^https://(?P<service>[^/]+)/(?P<org>[^/]+)/(?P<repo>[^/]+?)(.git)?$"),
+        re.compile(r"^https://(?P<service>[^/]+)/(?P<org>[^/]+)/(?P<repo>[^/]+?)(?:.git)?$"),
+        re.compile(r"^ssh://(?P<service>clark)/state/git/(?P<repo>[^/]+?)(?:.git)?$"),
     ]
 
     for regexp in regexps:
         if match := regexp.match(remote):
-            service = match.group('service')
-            org = match.group('org')
-            repo = match.group('repo')
-            return [service, org, repo]
+            return match.groups()
 
     return None
 
