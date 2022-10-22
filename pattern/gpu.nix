b5a17b95b271d96789b6a089acd7e1513277b477
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-10-22 10:37:38 -0700

    Drop nvidia support

--- pattern/gpu.nix
+++ pattern/gpu.nix
@@ -1,15 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  imports = [
-    ./nvidia-gpu.nix
-  ];
-
-  specialisation = {
-    intel-gpu.configuration = {
-      # A mobile setup that relies *only* upon the igpu. It cannot connect to any
-      # external displays, but is probably more power efficient.
-      services.xserver.videoDrivers = [ "modesetting" ];
-    };
+  # Intel graphics
+  # Enable hardware accelerated video playback
+  # (copied from https://nixos.wiki/wiki/Accelerated_Video_Playback)
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
   };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+
+  # Disabling PSR as a workaround for lag issues.
+  # See https://bbs.archlinux.org/viewtopic.php?id=279885
+  # and https://wiki.archlinux.org/title/intel_graphics#Screen_flickering
+  boot.kernelParams = [ "i915.enable_psr=0" ];
 }
