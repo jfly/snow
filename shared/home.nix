27098150a039383ab321c4bee2d4efe361753692
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-11-03 19:39:31 -0700

    Add missing file to fix `wallabag-add` script

--- shared/home.nix
+++ shared/home.nix
@@ -132,6 +132,55 @@ in
     # TODO: figure out how to get this config living closer to the
     # installation of docker itself.
     ".docker/config.json".text = docker-conf;
+
+    ".config/.wallabag.secrets".text = (
+      let
+        clientId = pkgs.deage.string ''
+          -----BEGIN AGE ENCRYPTED FILE-----
+          YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSAwaThYNXlOK213eG1CWXJo
+          L3lmY2FubFpZNSt5TjRNTHliOVIyd3JRNWpRClVjaEFId3pCZ0x6dUpNM0tNbVdC
+          N3V0enFhSm1PdzB1TzYxeE41K29DbjAKLS0tIDZvSUhyeFpOMlE3UnVTa2RXSEQx
+          VUMxejBvMG4yczRuMjNmbHZXMG1uc00K8cRL8BXiKSfxNW/hA7FGHfI1NOr+kake
+          1dD0jivvNdvaLKAqSYzNNGn3AQf8Rog5zHFsDJ/tZ4KoDNg1U0A5YxcRhGT1XG2A
+          57CSkBLsqJXXec0=
+          -----END AGE ENCRYPTED FILE-----
+        '';
+        clientSecret = pkgs.deage.string ''
+          -----BEGIN AGE ENCRYPTED FILE-----
+          YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBTUFJYZTYzcXFTb0FXMmxp
+          UFdMcE1mcGswR20vWDd3U3JsRllqbGtJTmljCjQ3K0dqTkhiRGVESnhUODd5R3dx
+          TUpNMjExZmx1Yi9TQTRTRGJNTiswOEEKLS0tIHMwUHRxeXpGNDl6RlVQMUkwbGs5
+          eDF2VFB4V0lta3dJUVFkRnZxbkdZUjAKYhmIWKfzrGSDX5wqcUHRGMAbBHkH/c7R
+          /GHCIdnYAgScpSqoZYuCLwuEyi52mUQwBcdPu+a0MCeRO8sW3FCPe5i1C74u+YsY
+          xRR7qslieC4E6A==
+          -----END AGE ENCRYPTED FILE-----
+        '';
+        username = pkgs.deage.string ''
+          -----BEGIN AGE ENCRYPTED FILE-----
+          YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSAxTkxWK3M0eXR3Ukd3eDd1
+          T000U3p2VmFlNjgvYUhiMGdYOTdwaFZuUXhZCkFiSjZtMVJlTytSckFmaGFlNmY5
+          L0UwUmdvVGE3WjVpYzBMMHFaRFl0RU0KLS0tIFpxRTlWUXhzUXVoMTY3cC9ZM2t2
+          MmVOa2ErdDdEY005RDlhNVRwcWNMV2cKDdBPq0i7XCStd08I3oLyYrwHM5Bu3GBJ
+          2/4s68mEBKzsugvN
+          -----END AGE ENCRYPTED FILE-----
+        '';
+        password = pkgs.deage.string ''
+          -----BEGIN AGE ENCRYPTED FILE-----
+          YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBOUTFJRjB2Y1VEWlBvL253
+          blpNdHF5WldieFR2ejVTMkJmeVk0a3Nob2pVClpQMGpSaC8wRkl6eUlxdGxDTGNQ
+          di82M3E4MGZFSzA0Z3lSaUZrMUFDbWMKLS0tIGtUT3NuZEh5UCtvcGcvbk1GanBW
+          SzMrc1JSR2dKMmZZRUNVTFpGTkdiNVUKoPhfYJ5kfy2/5X2g6tSS5tsgU+ckNb+E
+          Y4bXJtWzd1WadB3swYV6Wtp8DTxkC9A=
+          -----END AGE ENCRYPTED FILE-----
+        '';
+      in
+      ''
+        export WALLABAG_CLIENT_ID=${clientId}
+        export WALLABAG_CLIENT_SECRET=${clientSecret}
+        export WALLABAG_USERNAME=${username}
+        export WALLABAG_PASSWORD=${password}
+      ''
+    );
   };
 
   dconf.settings = {
