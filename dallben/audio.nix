c06cd04d503237bcb1437341e999e3e436023c6f
Jeremy Fleischman <jeremyfleischman@gmail.com>
2022-12-20 19:06:12 -0800

    Drop custom kernel: we don't need it anymore
    I recently upgraded us to the latest nixpkgs, which means we've got a
    new enough kernel

--- dallben/audio.nix
+++ dallben/audio.nix
@@ -1,11 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Use the latest kernel because the default (5.10) doesn't have this fix for
-  # sound cards on NUCs:
-  # https://github.com/torvalds/linux/commit/e81d71e343c6c62cf323042caed4b7ca049deda5
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
   # Enable audio
   hardware.pulseaudio.enable = true;
   users.users.${config.variables.kodiUsername}.extraGroups = [
