6c1e0047e5403d7ce16b688c207c842c17ffe5bd
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-04-07 23:55:26 -0700

    Port fflam from raspberry pi 4 to a thinkcentre m92p
    Some rando on the internet suggested this as a cheap, decent machine, so
    here we are giving it a shot!
    
    This was a nice reminder that I haven't totally ironed out how to
    bootstrap a new nixos machine, I'm looking forward to making that
    awesome someday. As a reminder, here's the current "state of the art":
    
    1. Build a bootable usb drive and start nixos installation by following
       these instructions
       https://nixos.org/manual/nixos/stable/index.html#ch-installation
    2. Do some manual hacking to enable ssh, and the pick up from my laptop
       using colmena in the snow repo.
    
    Note: there are some neat ideas here:
    https://github.com/zhaofengli/colmena/issues/42#issuecomment-1004528027
    to avoid this whole dance.
    
    I also went though a whole damn saga just to get the machine booting
    properly from UEFI. Turns out the bios it shiped with (from 2012) had a
    ridiculous behavior where it would *only* boot into boot entries labeled
    "Windows Boot Manager" or "Red Hat Enterprise Linux". Wtf.
    
    Copy pasting notes from my UEFI saga:
    
    EDIT: Nevermind, good news! Installing the latest bios update from
    https://support.lenovo.com/us/en/downloads/ds029265-flash-bios-update-thinkcentre-edge-92-thinkcentre-m82-m92-and-m92p-thinkstation-e31
    (as of 2022-04-07, that's version 9SKT9CA released on 2018-12-23) makes
    this stupidity go away =)
    
    First place I found talking about this:
    https://gist.github.com/vees/b3fb1e5b62da155a006831c16eaac8e8
    
    Good description of bug: https://mjg59.dreamwidth.org/20187.html
    How to fix it (because efibootmgr doesn't support editing an entry):
    https://www.reddit.com/r/ManjaroLinux/comments/e682d6/fixing_lenovos_error_code_1962_by_spoofing_the/
    
    How exactly I fixed it:
    
        efibootmgr -Bb 0000 efibootmgr -c -d /dev/sda2 -p 2 -L "Red Hat
        Enterprise Linux" -l "\EFI\systemd\systemd-bootx64.efi"
    
    Investigation into if we could tweak this in nixpkgs:
    
    here's the call to `bootctl`:
    https://github.com/jfly/nixpkgs/blob/4b11ab1f1238c8f3c00fa05af17b8b76c8211a7d/nixos/modules/system/boot/loader/systemd-boot/systemd-boot-builder.py#L258
    `man bootctl` shows that there's a "--efi-boot-option-description"
    parameter we could use

--- fflam/configuration.nix
+++ fflam/configuration.nix
@@ -1,62 +1,23 @@
 { config, lib, pkgs, ... }:
 
 {
-  nixpkgs.system = "aarch64-linux";
+  nixpkgs.system = "x86_64-linux";
 
   imports =
     [
+      ./hardware-configuration.nix
       ./nas.nix
       ./snow-backup.nix
     ];
 
-  boot = {
-    kernelPackages = pkgs.linuxPackages_rpi4;
-    tmpOnTmpfs = true;
-    initrd.availableKernelModules = [ "usbhid" "usb_storage" ];
-    # ttyAMA0 is the serial console broken out to the GPIO
-    kernelParams = [
-      "8250.nr_uarts=1"
-      "console=ttyAMA0,115200"
-      "console=tty1"
-      # Some gui programs need this
-      "cma=128M"
-    ];
+  # Use the systemd-boot EFI boot loader.
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
   };
-
-  # https://github.com/NixOS/nixpkgs/issues/154163#issuecomment-1008362877
-  nixpkgs.overlays = [
-    (final: super: {
-      makeModulesClosure = x:
-        super.makeModulesClosure (x // { allowMissing = true; });
-    })
-  ];
-
-  # I had a lot of trouble getting an sdcard to build, and then ran into
-  # different problems when trying to deploy remotely.
-  # https://discourse.nixos.org/t/raspberry-pi-boot-loader-raspberrypi-firmwareconfig-not-taking-effect/19692/3
-  # https://discourse.nixos.org/t/raspberrypi-boot-loader-still-using-extlinux/20119/2
-  # https://github.com/NixOS/nixpkgs/issues/173948
-  #
-  # Copied from nixos/modules/installer/sd-card/sd-image-raspberrypi.nix
-  boot.loader.grub.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = true;
-  # I believe this `boot.loader.raspberryPi` stuff is deprecated? Although I
-  # can't find a good source to say that.
-  # It certainly doesn't play nicely with the sd card generation code in
-  # nixos/modules/installer/sd-card/sd-image-aarch64.nix.
-  # boot.loader.raspberryPi = {
-  #   enable = true;
-  #   version = 4;
-  # };
-
-  # Required for the Wireless firmware
-  hardware.enableRedistributableFirmware = true;
 
   networking = {
     hostName = "fflam";
-    networkmanager = {
-      enable = true;
-    };
     # Disable the firewall. I'm just not used to having one, and we're behind a NAT anyways...
     firewall.enable = false;
   };
@@ -81,29 +42,11 @@
     '';
   };
 
-  # Assuming this is installed on top of the disk image.
-  fileSystems = {
-    "/boot" = {
-      device = "/dev/disk/by-label/FIRMWARE";
-      fsType = "vfat";
-    };
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
-  };
-
-  nixpkgs.config = {
-    allowUnfree = true;
-  };
-  powerManagement.cpuFreqGovernor = "ondemand";
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
+  system.stateVersion = "22.11"; # Did you read the comment?
 }
