169764cce5987f31b4c29344fb9691226e491c73
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-05-22 15:57:36 -0700

    Add another whoami to debug stuff behind sso

--- k8s-pulumi/app/whoami.py
+++ k8s-pulumi/app/whoami.py
@@ -11,3 +11,10 @@ class Whoami:
             # It's ok to expose this to the outside world. Nothing scary here!
             access=Access.INTERNET_UNSECURED,
         )
+        snowauth.declare_app(
+            name="whoami-sso",
+            namespace="default",
+            image="containous/whoami:latest",
+            port=80,
+            access=Access.INTERNET_BEHIND_SSO,
+        )
