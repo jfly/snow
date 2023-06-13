971356ea1fc0729f5aace8945971f96d8b38b5b6
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-06-13 09:12:03 -0700

    Clear out `/tmp` on each boot

--- pattern/configuration.nix
+++ pattern/configuration.nix
@@ -59,6 +59,10 @@ in
   # https://nixos.wiki/wiki/Bootloader#Limiting_amount_of_entries_with_grub_or_systemd-boot
   boot.loader.systemd-boot.configurationLimit = 50;
   boot.loader.efi.canTouchEfiVariables = true;
+  # nixos doesn't clear out /tmp on each boot. I'm used to it being a tmpfs
+  # (`boot.tmp.useTmpfs = true`), but nix-shell uses it, and it needs a *lot*
+  # of space, and I'm not sure I want to allocate that much ram?
+  boot.tmp.cleanOnBoot = true;
 
   swapDevices = [{ device = "/swapfile"; size = 2048; }];
 
