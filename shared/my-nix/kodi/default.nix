214c788ec09deaf4d53b4210a2ad133ef0ee6b12
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-06-14 11:59:03 -0700

    Camelcasing

--- shared/my-nix/kodi/default.nix
+++ shared/my-nix/kodi/default.nix
@@ -2,15 +2,15 @@
 
 let
   media = pkgs.callPackage ./media { };
-  my_kodi_with_packages = pkgs.kodi.withPackages (p: [ p.a4ksubtitles media ]);
+  myKodiWithPackages = pkgs.kodi.withPackages (p: [ p.a4ksubtitles media ]);
 
   # This is unfortunate: it just doesn't seem to be possible to set some kodi
   # settings without creating files in the ~/.kodi/userdata/addon_data
   # directory. So, we wrap kodi to give us an opportunity to do that.
   genKodiAddonData = pkgs.callPackage ./gen-kodi-addon-data { };
-  my_kodi = pkgs.symlinkJoin {
+  myKodi = pkgs.symlinkJoin {
     name = "kodi";
-    paths = [ my_kodi_with_packages ];
+    paths = [ myKodiWithPackages ];
     buildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
       wrapProgram $out/bin/kodi \
@@ -19,4 +19,4 @@ let
   };
 in
 
-my_kodi
+myKodi
