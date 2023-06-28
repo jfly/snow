c0d1e0af673ce972625037a4d422194a410bc711
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-06-28 10:24:56 -0700

    Try increasing the boot configuration limit
    I've increated my /boot partition to 1 GiB, so hopefully it can hold
    this many configurations.

--- pattern/configuration.nix
+++ pattern/configuration.nix
@@ -57,7 +57,7 @@ in
   # Keep only a finite number of boot configurations. This prevents /boot from
   # filling up.
   # https://nixos.wiki/wiki/Bootloader#Limiting_amount_of_entries_with_grub_or_systemd-boot
-  boot.loader.systemd-boot.configurationLimit = 25;
+  boot.loader.systemd-boot.configurationLimit = 100;
   boot.loader.efi.canTouchEfiVariables = true;
   # nixos doesn't clear out /tmp on each boot. I'm used to it being a tmpfs
   # (`boot.tmp.useTmpfs = true`), but nix-shell uses it, and it needs a *lot*
