555274f3bfb34b2848f4c46e22d559a4f548c0c2
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-07-01 15:30:14 -0700

    Downgrade to nvim <0.9
    This works around an annoying issue (see link in diff, I'm not putting
    it here in order to avoid spamming the github issue with noise)

--- shared/my-nix/nvim/default.nix
+++ shared/my-nix/nvim/default.nix
@@ -42,71 +42,83 @@ let
     " Override the VIM definition
     call tcomment#type#Define('vim', '"${conflictMarker} %s')
   '';
+  # Downgrade to neovim 8 as a workaround for https://github.com/neovim/neovim/issues/21064
+  # Generated from
+  # https://lazamar.co.uk/nix-versions/?package=neovim&version=0.8.3&fullName=neovim-0.8.3&keyName=neovim&revision=8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8&channel=nixpkgs-unstable#instructions
+  oldNixpkgs = import
+    (builtins.fetchGit {
+      name = "nixpkgs-with-neovim-8";
+      url = "https://github.com/NixOS/nixpkgs/";
+      ref = "refs/heads/nixpkgs-unstable";
+      rev = "8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8";
+    })
+    {
+      localSystem = pkgs.system;
+    };
+  neovim_8 = oldNixpkgs.neovim-unwrapped;
 in
 
-pkgs.neovim.override {
-  vimAlias = true;
-  viAlias = true;
-  configure = {
-    packages.myPackages = with pkgs.vimPlugins; {
-      start = [
-        fzf-vim
-        MatchTagAlways
-        matchit-zip
-        # Tweak tcomment so comments for (nearly) all languages get the
-        # conflict marker characters I'm so used to having.
-        (tcomment_vim.overrideAttrs (oldAttrs: {
-          preInstall = ''
-            # Some clever regexes to try to replace all the comment
-            # strings in
-            # https://github.com/tomtom/tcomment_vim/blob/master/autoload/tcomment/types/default.vim
-            # This isn't perfect.
-            f=autoload/tcomment/types/default.vim
-
-            # Match simple lines like:
-            #     call tcomment#type#Define('aap', '# %s')
-            sed -i "s/\(Define('.*', *'\S\+\)\( %s.*\)/\1${conflictMarker}\2/" $f
-
-            # Match lines like:
-            #     call tcomment#type#Define('cpp', tcomment#GetLineC('// %s'))
-            sed -i "s/\(tcomment#GetLineC('\S\+\)\( %s\)/\1${conflictMarker}\2/" $f
-
-            # Match lines like:
-            #     call tcomment#type#Define('clojure', {'commentstring': '; %s', 'count': 2})
-            sed -i "s/\('commentstring': \+'\S\+\)\( %s\)/\1${conflictMarker}\2/" $f
-
-            cat ${tcommentOverrides} >> $f
-          '';
-        }))
-
-        # Syntax highlighting + colors
-        vim-dim
-        vim-polyglot
-
-        lightline-vim
-        lightline-bufferline
-        vim-fugitive
-        vim-rhubarb
-        vim-test
-        traces-vim
-        vim-rsi # readline shortcuts in useful places
-        vim-mergetool
-        (pkgs.vimUtils.buildVimPlugin {
-          pname = "honorjs-test-runner";
-          version = "0.0.1";
-          src = ./plugin;
-        })
-
-        # Linting/autofixing/LSP, etc
-        editorconfig-vim
-        ale
-        nvim-lspconfig
-        cmp-nvim-lsp
-        cmp-buffer
-        nvim-cmp
-        null-ls-nvim
-      ];
-    };
+pkgs.wrapNeovimUnstable neovim_8 (
+  pkgs.neovimUtils.makeNeovimConfig {
+    vimAlias = true;
+    viAlias = true;
     customRC = builtins.readFile ./vimrc;
-  };
-}
+    plugins = with pkgs.vimPlugins; [
+      fzf-vim
+      MatchTagAlways
+      matchit-zip
+      # Tweak tcomment so comments for (nearly) all languages get the
+      # conflict marker characters I'm so used to having.
+      (tcomment_vim.overrideAttrs (oldAttrs: {
+        preInstall = ''
+          # Some clever regexes to try to replace all the comment
+          # strings in
+          # https://github.com/tomtom/tcomment_vim/blob/master/autoload/tcomment/types/default.vim
+          # This isn't perfect.
+          f=autoload/tcomment/types/default.vim
+
+          # Match simple lines like:
+          #     call tcomment#type#Define('aap', '# %s')
+          sed -i "s/\(Define('.*', *'\S\+\)\( %s.*\)/\1${conflictMarker}\2/" $f
+
+          # Match lines like:
+          #     call tcomment#type#Define('cpp', tcomment#GetLineC('// %s'))
+          sed -i "s/\(tcomment#GetLineC('\S\+\)\( %s\)/\1${conflictMarker}\2/" $f
+
+          # Match lines like:
+          #     call tcomment#type#Define('clojure', {'commentstring': '; %s', 'count': 2})
+          sed -i "s/\('commentstring': \+'\S\+\)\( %s\)/\1${conflictMarker}\2/" $f
+
+          cat ${tcommentOverrides} >> $f
+        '';
+      }))
+
+      # Syntax highlighting + colors
+      vim-dim
+      vim-polyglot
+
+      lightline-vim
+      lightline-bufferline
+      vim-fugitive
+      vim-rhubarb
+      vim-test
+      traces-vim
+      vim-rsi # readline shortcuts in useful places
+      vim-mergetool
+      (pkgs.vimUtils.buildVimPlugin {
+        pname = "honorjs-test-runner";
+        version = "0.0.1";
+        src = ./plugin;
+      })
+
+      # Linting/autofixing/LSP, etc
+      editorconfig-vim
+      ale
+      nvim-lspconfig
+      cmp-nvim-lsp
+      cmp-buffer
+      nvim-cmp
+      null-ls-nvim
+    ];
+  }
+)
