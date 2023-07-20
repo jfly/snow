d0e07b76850b4b303aed2eba3e7816c776a1ebf4
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-07-20 03:07:37 -0700

    Remove remaining uses of deage for pattern
    From
    https://docs.syncthing.net/users/faq.html#should-i-keep-my-device-ids-secret:
    
    > Should I keep my device IDs secret?
    >
    > No. The IDs are not sensitive
    
    Granted, not everyone agrees about this (e.g., [this
    thread](https://forum.syncthing.net/t/censor-device-ids-in-the-logs/14824)),
    but I don't think the concerns raised there worry me :shrug:.

--- pattern/syncthing.nix
+++ pattern/syncthing.nix
@@ -21,56 +21,23 @@ in
       overrideFolders = true;
       devices = {
         "snow" = {
-          id = pkgs.deage.string ''
-            -----BEGIN AGE ENCRYPTED FILE-----
-            YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBLeUxtZlk3OGZyQkMrZzlm
-            YjJvRmJOdlZNMU5EakJNOTJZQ2hsdmEvZlMwClNJNkRHQU5QTWhNekQzRzZuTlEx
-            bGVDL1Fyd2F2L05jbUUyRzRqdERsclUKLS0tIDViSTVZSHJzejdMdHVEeGg5ZzND
-            NmUzUGNncTVTSHhsaVIxc2pCeURCdlkKngDZxrehlssfMTwDxHNkndju4K9i3ObJ
-            PBUjBa5TsMS61TxqEquoQb7GX3Rg+tX373//AXsm/ZrbU3H/16SE4GT0P+rAVwpV
-            JlK+yfB/Q2E5YI6Pei2kTdENXyZIqe0=
-            -----END AGE ENCRYPTED FILE-----
-          '';
+          id = "D3NFS4D-DHERIM7-T62ZVKD-RE6T37K-MCYWHDE-CHH56RW-E6KN4HF-TDWVXAR";
         };
       };
       folders = {
         "music" = {
           type = "receiveonly";
-          id = pkgs.deage.string ''
-            -----BEGIN AGE ENCRYPTED FILE-----
-            YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBVWVVDWkxBeDNnbER5RFVS
-            NTNkRlM4TkZTSVEzVDdrSjlPUVltTkJ6bm4wCmVTbTlUaVYrUWhEMThIZVNUVEJm
-            c0N0SW9jY3BwdHZrbHp0bWFRbGtYaTQKLS0tIG1JaFN1ajFDbkZ1VUlWQmVPYllO
-            ZlNLVWExV28zZHlqRVNkSjB2WHUyaFkKB0d71Vb1UlmbH03Fzr6SfBGK7M2zRMKz
-            opQRNznVTe8LDFMy3wbTusjnBA==
-            -----END AGE ENCRYPTED FILE-----
-          '';
+          id = "wgvgw-yqwcq";
           path = "${syncDir}/music";
           devices = [ "snow" ]; # Which devices to share the folder with
         };
         "calibre" = {
-          id = pkgs.deage.string ''
-            -----BEGIN AGE ENCRYPTED FILE-----
-            YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBTcjMreGN3NFJIZ0RqN2Nk
-            SDc0V24vLzBwbE1FSTI5dGVXSUdWdzdsdENnClI4VnY3WG9EVlo5blpmZFRMS0hH
-            TjhKQlhSckJOK0NORyt5aHJVeituU2sKLS0tIGxnclBDNUIzRkN0UEh6cUxHbEhT
-            YVFGaE9na3ZycnAyU3NJaGk0TEcxY2sKNXfyiFrQi9bzTd2ca+LMvLr1lGh8y8Ny
-            /OF/t7JuvBy+zeIO9g6nQ1cKxQ==
-            -----END AGE ENCRYPTED FILE-----
-          '';
+          id = "ahnvm-wqudj";
           devices = [ "snow" ];
           path = "${syncDir}/calibre";
         };
         "scratch" = {
-          id = pkgs.deage.string ''
-            -----BEGIN AGE ENCRYPTED FILE-----
-            YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBCTmJhMHQ2aFdYa2RxRlVj
-            eFpWUEpMZHFXSytJKzgzWUhSMUw2bHNQU0Q0CldpYkp5K2dMWXBoWGhWY1RuT0Zk
-            dXNGV3dOcytSSkhvYTZjTWZBUEkrL3MKLS0tIDFBUG5OY1ZTSVhFTU1iQWoxVVlh
-            VXc0WnB2K25uUFVIMlBRL1lDTzFEdVUKtfXgDzEJv39ePZFn8dqxh+2JdnvuPs69
-            OOA2njc8xXPYiQO4bSIU+fVuCQ==
-            -----END AGE ENCRYPTED FILE-----
-          '';
+          id = "etyx6-oh4ft";
           devices = [ "snow" ];
           ignorePerms = false; # By default, Syncthing doesn't sync file permissions, but there are some scripts in here.
           path = "${syncDir}/scratch";
