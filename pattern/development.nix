jfly/snow

commit 72a439edf8eefc0a91fad8c0353d8661b5c76c52

Fix some longstanding dns issues
I just did a ton of reading about dns. It turns out the configuration I
had here did not work. I *thought* it worked, because I used
dig/nslookup to test things out, but many applications use glibc to do
DNS lookups, which honors `/etc/nsswitch`, which has `hosts: ... resolve
...`, which sends requests to systemd-resolved over dbus. Now, we let
systemd-resolved create its stub listener (and we reconfigure dnsmasq to
not conflict with it).

I also did a bunch of reading on how to keep my corp vpn from
conflicting with my local DNS, and I found some clues, but not an
end-to-end answer yet. I've filed an issue with openvpn3-linux seeking
help (see the link down below). For now, my workaround is to just
completely ignore my corp dns.

pattern/development.nix
@@ -138,16 +138,19 @@ in
     programs.openvpn3.enable = true;
     # With systemd-resolved running, openvpn3 won't stomp on /etc/resolv.conf.
     services.resolved.enable = true;
-    # Disable systemd-resolved resolver to avoid a port conflict with dnsmasq.
-    services.resolved.extraConfig = ''
-      DNSStubListener=no
-    '';
 
     # Set up a local DNS server
-    networking.resolvconf.useLocalResolver = true;
     services.dnsmasq = {
       enable = true;
+      # Configure the system to actually *use* dnsmasq (in this case, this
+      # updates systemd-resolved to use 127.0.0.1 as a DNS resolver).
+      resolveLocalQueries = true;
       settings = {
+        # Only let dnsmasq bind on the loopback interface. This prevents a port
+        # conflict with systemd-resolved.
+        interface = "lo";
+        bind-interfaces = true;
+
         address = [
           "/local.honor/127.0.0.1"
           # See notes above about `virtualisation.docker.daemon.settings.dns` to

shared/homies/bin/h4vpn
@@ -19,4 +19,12 @@ function toggle() {
     fi
 }
 
+# Note: I've manually edited this .ovpn file to ignore the pushed DNS server.
+# However, it should be possible to instead configure things so we only use
+# that DNS server for some domains. See
+# https://github.com/OpenVPN/openvpn3-linux/issues/208 for a question I filed
+# about this.
+# If we don't get an answer there, we could implement this with a custom script.
+# See script documentation here: https://github.com/OpenVPN/openvpn3-linux/blob/master/docs/man/openvpn2.1.rst#script-execution
+# The command to run will be something like: `sudo resolvectl domain tun0 '~honorcare.com'`.
 toggle ~/sync/linux-secrets/h4-vpn/ovpn-access-server.ovpn

commit ec956e81a721b8869636914096a344a8c9a1f2f5

Fix docker networking
Oops, I broke DNS in containers in
https://github.com/jfly/snow/commit/72a439edf8eefc0a91fad8c0353d8661b5c76c52

This was was sort of subtle! Somewhat relevant:
https://superuser.com/questions/1702091/how-should-systemd-resolved-and-docker-interact.

Docker is smart enough to detect that we're using sytemd-resolved. It
then pulls our DNS servers from /run/systemd/resolve/resolv.conf, which
includes dnsmasq, which was previously *only* listening to the loopback
interface, and thereby ignoring all dns queries coming from the docker
network. I added the docker network to the list, and all is well.

See comment below about an alternative solution involing `bind-dynamic`
that might be preferable if this list gets annoying to maintain.

pattern/development.nix
@@ -146,9 +146,14 @@ in
       # updates systemd-resolved to use 127.0.0.1 as a DNS resolver).
       resolveLocalQueries = true;
       settings = {
-        # Only let dnsmasq bind on the loopback interface. This prevents a port
-        # conflict with systemd-resolved.
-        interface = "lo";
+        # Only let dnsmasq bind on specific interfaces. This keeps dnsmasq from
+        # trying to bind to a wildcard address, which conflicts with the
+        # 127.0.0.54 that sytsemd-resolved listens on.
+        # Note: if this list is annoying to keep up to date, we could instead
+        # use `bind-dynamic = true;`, which listens for all interfaces as they
+        # come and go, and also won't use a wildcard address, so it places
+        # nicely with systemd-resolved.
+        interface = [ "lo" "docker0" ];
         bind-interfaces = true;
 
         address = [