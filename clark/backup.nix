8e8220c01d35ab25d243e6f9fb4b2abac8d03332
Jeremy Fleischman <jeremyfleischman@gmail.com>
2023-11-12 12:51:33 -0800

    Vaultwarden hardening and transactionally consistent backups

--- clark/backup.nix
+++ clark/backup.nix
@@ -8,6 +8,18 @@ let
     # Ensure these files are read + writeable by group.
     umask 002
 
+    # First, do some special handling of anything like a database that needs a
+    # transactionally consistent view of the world.
+    # TODO: this doesn't handle our various mysql and postgresql dbs. Rather
+    # than have this grow to support all of those, figure out a more
+    # distributed way of handling this. Some ideas:
+    #  - Do this in a k8s cron job for each thing that needs it.
+    #  - Switch to some crazy filesystem that does have support for atomic
+    #    backups
+    #    (https://www.reddit.com/r/sysadmin/comments/7fyn74/lvm_btrfs_zfs_what_is_the_best_solution_for/)
+    mkdir -p /state/transactionally-consistent
+    ${pkgs.sqlite}/bin/sqlite3 /state/vaultwarden/db.sqlite3 ".backup '/state/transactionally-consistent/vaultwarden.sqlite'"
+
     # Create a backup with today's date
     backup=/mnt/media/backups/clark-state-$(date -I).tar
     wip_backup=$backup.wip
@@ -20,8 +32,6 @@ let
 in
 {
   # Do a daily backup of /state
-  # TODO: also do mysqldump/pg_dump to deal with the fact that this is not
-  # atomic and databases are probably finnicky.
   systemd = {
     timers.state-backup = {
       description = "/state backup timer";
--- k8s-pulumi/app/vaultwarden.py
+++ k8s-pulumi/app/vaultwarden.py
@@ -1,23 +1,42 @@
 import pulumi_kubernetes as kubernetes
 from .snowauth import Snowauth, Access
+from .deage import deage
 
 
 class Vaultwarden:
     def __init__(self, snowauth: Snowauth):
+        # We host Vaultwarden under a secret subpath as a form of defense in
+        # depth. See
+        # https://github.com/dani-garcia/vaultwarden/wiki/Hardening-Guide#hiding-under-a-subdir
+        super_secret_subpath = deage(
+            """
+            -----BEGIN AGE ENCRYPTED FILE-----
+            YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBzdXA4YlNBRlNyaEY5RkFs
+            TjFCNjVoWFh0MFh4R0JVcGhvT0dWS2pOdms0Ckgwc1EyQmR0WlNlcWo3cUdwb2RD
+            MVBBelEzclc5N1BUaXdSUU00WldySFEKLS0tIHJsR05vUHhuYmRRL2dkWlo4Unpt
+            OVNsQTlVOFBFMm54LzZUaGtUckx4cVEK9xOT3m0++iI2bF/TLoWAhONVd3/I/zrP
+            YjIU9a6kq2+0/9HuOPPdEs9C
+            -----END AGE ENCRYPTED FILE-----
+            """
+        )
+        domain = f"https://vw.snow.jflei.com/{super_secret_subpath}"
+
         snowauth.declare_app(
             name="vw",
             namespace="default",
             image="vaultwarden/server:latest",
             port=80,
             env={
-                "DOMAIN": "https://vw.snow.jflei.com",
-                # https://github.com/dani-garcia/vaultwarden/wiki/Disable-registration-of-new-users
+                "DOMAIN": domain,
+                # https://github.com/dani-garcia/vaultwarden/wiki/Hardening-Guide#disable-registration-and-optionally-invitations
                 "SIGNUPS_ALLOWED": "false",
+                "INVITATIONS_ALLOWED": "false",
+                # https://github.com/dani-garcia/vaultwarden/wiki/Hardening-Guide#disable-password-hint-display
+                "SHOW_PASSWORD_HINT": "false",
             },
-            # Out of an abundance of caution, for now we only allow access to
-            # Vaultwarden when you're on the local network.
-            # https://github.com/dani-garcia/vaultwarden/wiki/Running-a-private-vaultwarden-instance-with-Let%27s-Encrypt-certs
-            access=Access.LAN_ONLY,
+            # This is ok to expose "unsecured", as bitwarden has its own login
+            # flow.
+            access=Access.INTERNET_UNSECURED,
             volume_mounts=[
                 kubernetes.core.v1.VolumeMountArgs(
                     mount_path="/data",
@@ -35,6 +54,4 @@ class Vaultwarden:
                 ),
             ],
         )
-        # TODO: enable websocket notifications: https://github.com/dani-garcia/vaultwarden/wiki/Enabling-WebSocket-notifications
-        # TODO: consider disabling password hints? https://github.com/dani-garcia/vaultwarden/wiki/Hardening-Guide#disable-password-hint-display
         # TODO: consider more forms of backup: https://github.com/dani-garcia/vaultwarden/wiki/Backing-up-your-vault
