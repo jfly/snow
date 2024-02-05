1a319b5d6d00717f5c1416eb670720182235c173
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-02-04 23:30:45 -0800

    Add fetcher

--- clark/pr-tracker.nix
+++ clark/pr-tracker.nix
@@ -5,9 +5,13 @@ let
   pgPort = config.services.postgresql.port;
   port = 7000;
   user = "pr-tracker";
+  databaseUrl = "postgresql:///${user}?host=/run/postgresql&port=${builtins.toString pgPort}";
 in
 {
-  imports = [ pr-tracker.nixosModules.api ];
+  imports = [
+    pr-tracker.nixosModules.api
+    pr-tracker.nixosModules.fetcher
+  ];
 
   services.postgresql.enable = true;
   services.postgresql.ensureDatabases = [ user ];
@@ -22,6 +26,19 @@ in
   services.pr-tracker-api.enable = true;
   services.pr-tracker-api.port = port;
   services.pr-tracker-api.user = user;
-  services.pr-tracker-api.databaseUrl = "postgresql:///${user}?host=/run/postgresql&port=${builtins.toString pgPort}";
+  services.pr-tracker-api.group = "pr-tracker";
+  services.pr-tracker-api.databaseUrl = databaseUrl;
   services.pr-tracker-api.localDb = true;
+
+  services.pr-tracker-fetcher.enable = true;
+  services.pr-tracker-fetcher.user = user;
+  services.pr-tracker-fetcher.group = "pr-tracker";
+  systemd.services.pr-tracker-fetcher.environment.RUST_LOG = "info";
+  services.pr-tracker-fetcher.branchPatterns = [ "*" ];
+  services.pr-tracker-fetcher.databaseUrl = databaseUrl;
+  services.pr-tracker-fetcher.localDb = true;
+  services.pr-tracker-fetcher.githubApiTokenFile = "/run/pr-tracker-secrets/github.token"; # TODO: encrypt + version control this
+  services.pr-tracker-fetcher.repo.owner = "NixOS";
+  services.pr-tracker-fetcher.repo.name = "nixpkgs";
+  services.pr-tracker-fetcher.onCalendar = "daily";
 }
