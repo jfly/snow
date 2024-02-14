881d3bc90a2e86a1c56ffa0241c4b230c6e501c6
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-02-13 20:22:55 -0800

    Add poetry 1.6.0

--- shared/asdf-nix/poetry.nix
+++ shared/asdf-nix/poetry.nix
@@ -28,6 +28,17 @@ let
       localSystem = pkgs.system;
     }).poetry;
 
+  poetry160 = (import
+    (builtins.fetchGit {
+      name = "nixpkgs-with-poetry-1.6.0";
+      url = "https://github.com/jfly/nixpkgs/";
+      ref = "poetry-1.6.0";
+      rev = "8a55bc6f690071108781a3940674726b43a475ef";
+    })
+    {
+      localSystem = pkgs.system;
+    }).poetry;
+
   poetry161 = (import
     (builtins.fetchGit {
       name = "nixpkgs-with-poetry-1.6.1";
@@ -53,6 +64,7 @@ let
   derivationByVersion = {
     "1.5.0" = poetry150;
     "1.5.1" = poetry151;
+    "1.6.0" = poetry160;
     "1.6.1" = poetry161;
     "1.7.1" = poetry171;
   };
