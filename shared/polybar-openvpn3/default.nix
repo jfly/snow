d1939610a6d999597e76f8554b664dc140ed1da2
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-03-25 09:12:13 -0700

    Revert "Upgrade to the latest version of openvpn3"
    This reverts commit 6a1adb0aa39aee9ccda46de659897e6b813181b0.
    
    Unfortunately, openvpn3 v21 has some bugs with DNS search domains, see
    https://github.com/OpenVPN/openvpn3-linux/issues/208#issuecomment-1807097626
    for details.

--- shared/polybar-openvpn3/default.nix
+++ shared/polybar-openvpn3/default.nix
@@ -8,5 +8,15 @@ with pkgs.python3Packages; buildPythonApplication {
   nativeBuildInputs = [ setuptools ];
   src = ./.;
 
-  propagatedBuildInputs = pkgs.openvpn3.pythonPath ++ [ pkgs.openvpn3 ];
+  propagatedBuildInputs = pkgs.openvpn3.pythonPath ++ [
+    (pkgs.openvpn3.overrideAttrs (oldAttrs: {
+      patches = [
+        # TODO: remove this when v21 of openvpn3 lands on nixpkgs-unstable.
+        (pkgs.fetchpatch {
+          url = "https://github.com/OpenVPN/openvpn3-linux/commit/ba6fe37e7e28d1e633b56052383da3072f03c11e.patch";
+          sha256 = "sha256-MBXDEfeyg0VQGp9GYcpTZyLB0h6LX1qlaqZSDhOAJgQ=";
+        })
+      ];
+    }))
+  ];
 }
