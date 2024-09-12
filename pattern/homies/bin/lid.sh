05663a43e1e68c92f9e563954a8a919ab3f699d1
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-09-12 11:44:45 -0700

    Some cottage cleanup

--- pattern/homies/bin/lid.sh
+++ pattern/homies/bin/lid.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if grep -q close /proc/acpi/button/lid/*/state; then
-    if nmcli -t connection show --active | cut -d : -f 1 | grep "^\(Cal 3\|Cal 3.5\|Cal\|Cal4\|Hen Wen\|Auto Ethernet\|Wired connection 1\)$" &>/dev/null; then
+    if nmcli -t connection show --active | cut -d : -f 1 | grep "^\(Cal 3\|Cal 3.5\|Cal\|Cal4\|Cal 5g\|Hen Wen\|Auto Ethernet\|Wired connection 1\)$" &>/dev/null; then
         echo "Ignoring lid close event because we're connected to: $(nmcli -t connection show --active | cut -d : -f 1)"
     else
         echo "Lid is closed. Locking screen."
