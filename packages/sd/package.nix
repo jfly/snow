8084358fa420184fb980a43fb4b65ad89ecfc7e9
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-09-28 16:15:26 -0500

    Add a sd patch with support for fish

--- packages/sd/package.nix
+++ packages/sd/package.nix
@@ -1,33 +1,26 @@
-{
-  pkgs,
-  lib,
-  stdenv,
-}:
+{ pkgs }:
 
-stdenv.mkDerivation rec {
-  pname = "sd";
-  version = "1.1.0";
-
+pkgs.script-directory.overrideAttrs (oldAttrs: {
+  # There hasn't been a release of sd in a while, but `main` has moved on a bit.
+  version = "1.1.0-unstable-2024_03_19";
   src = pkgs.fetchFromGitHub {
     owner = "ianthehenry";
     repo = "sd";
-    rev = "v${version}";
-    sha256 = "sha256-X5RWCJQUqDnG2umcCk5KS6HQinTJVapBHp6szEmbc4U=";
+    rev = "8a2335f765720afe01c7eca1ffbba669a66cadca";
+    hash = "sha256-pp5fq+0+BaKs2ZXio5ZIyXPUQrEhE5ziNBbyTIs5FDg=";
   };
 
-  strictDeps = true;
-  buildInputs = [ pkgs.bash ];
+  patches = [
+    # This PR adds Fish completions: <https://github.com/ianthehenry/sd/pull/18>
+    (pkgs.fetchurl {
+      url = "https://patch-diff.githubusercontent.com/raw/ianthehenry/sd/pull/18.patch";
+      hash = "sha256-u2Ovh2t2ev3kbxWjHe4OLdjdeIwcew4P1SsyPu7YQAU=";
+    })
+  ];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp sd $out/bin/sd
-    mkdir -p $out/share/zsh/site-functions
-    cp _sd $out/share/zsh/site-functions/_sd
-  '';
-
-  meta = {
-    description = "sd: my script directory";
-    homepage = "https://github.com/ianthehenry/sd";
-    license = lib.licenses.mit;
-  };
-}
+  installPhase =
+    oldAttrs.installPhase
+    + ''
+      installShellCompletion sd.fish
+    '';
+})
