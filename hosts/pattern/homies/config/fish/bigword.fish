ce4593fda5d951b233922f4c6e433f6b51a4ec54
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-09-30 13:45:43 -0500

    More tweaks to word navigation/word killing

--- hosts/pattern/homies/config/fish/bigword.fish
+++ hosts/pattern/homies/config/fish/bigword.fish
@@ -1,5 +1,6 @@
-bind \ed kill-bigword
+# Alt+Backspace kills a bigword
 bind \e\x7f backward-kill-bigword
-bind \e\b backward-kill-bigword
-bind \eb backward-bigword
-bind \ef forward-bigword
+
+# Ctrl+w kills a smaller word (Fish defaults this to
+# `backward-kill-path-component`, but that causes asymmetry with Alt+f/Alt+b
+bind \cw backward-kill-word
