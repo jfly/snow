8779cf3e4a3463b8e62cfc5836c98067f9e11c09
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-10-12 08:59:00 -0500

    Inline xmonad config, use alacritty instead of xterm

--- nixos-modules/xmonad-basic/default.nix
+++ nixos-modules/xmonad-basic/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
   services.xserver = {
     enable = true;
@@ -9,7 +9,33 @@
         hp.xmonad-contrib
         hp.xmonad-extras
       ];
-      config = builtins.readFile ./xmonad.hs;
+      config =
+        # haskell
+        ''
+          import           XMonad
+          import           XMonad.Config.Desktop
+          import           XMonad.Hooks.EwmhDesktops
+          import           XMonad.Layout.NoBorders
+
+
+          myLayout = smartBorders $ Full ||| tiled
+            where
+               -- default tiling algorithm partitions the screen into two panes
+               tiled   = Tall nmaster delta ratio
+               -- The default number of windows in the master pane
+               nmaster = 1
+               -- Default proportion of screen occupied by master pane
+               ratio   = 1/2
+               -- Percent of screen to increment by when resizing panes
+               delta   = 3/100
+
+
+          main = xmonad $ ewmh desktopConfig
+              { terminal    = "${lib.getExe pkgs.alacritty}"
+              , modMask     = mod4Mask
+              , layoutHook  = myLayout
+              }
+        '';
     };
   };
 }
