e118707d26d9549b050f602dce0fb2216927c758
Jeremy Fleischman <jeremyfleischman@gmail.com>
2024-12-11 13:20:24 -0800

    Test out some neovim patches

--- packages/neovim/package.nix
+++ packages/neovim/package.nix
@@ -11,7 +11,22 @@ inputs.nixvim.legacyPackages.${system}.makeNixvimWithModule {
   inherit pkgs;
 
   module = {
-    package = inputs'.neovim-nightly-overlay.packages.default;
+    package = inputs'.neovim-nightly-overlay.packages.default.overrideAttrs (oldAttrs: {
+      patches = oldAttrs.patches ++ [
+        # feat(diagnostic): store a unique quickfix list per title #31486
+        # https://github.com/neovim/neovim/pull/31486
+        (pkgs.fetchpatch {
+          url = "https://patch-diff.githubusercontent.com/raw/neovim/neovim/pull/31553.patch";
+          hash = "sha256-ijkC46FBuopOhdLIAiBx1Whl7+IfpU9Hh+RBaefD6o4=";
+        })
+        # feat(diagnostic): update existing quickfix list with title if possible
+        # https://github.com/neovim/neovim/pull/31486
+        (pkgs.fetchpatch {
+          url = "https://patch-diff.githubusercontent.com/raw/neovim/neovim/pull/31486.patch";
+          hash = "sha256-wSKMwHaQ58jPzBI8pNt2rvP5Ki1a2lwqVG4ubb+GIIA=";
+        })
+      ];
+    });
     _module.args.flake' = flake';
 
     viAlias = true;
