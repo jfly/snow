84080d895dd141b688d6acbe573b6ffb39313637
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-01-29 10:20:31 +0900

    Ignore lid close if external display is connected

--- hosts/pattern/homies/bin/lid.sh
+++ hosts/pattern/homies/bin/lid.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 
+# Consider porting this to `autoperipherals`.
+
+set -euo pipefail
+
+if xrandr | grep -Po '[^ ]+(?= connected)' | grep -v eDP-1 >/dev/null; then
+    echo "External display is connected. Doing nothing."
+    exit 0
+fi
+
 if grep -q close /proc/acpi/button/lid/*/state; then
-    if nmcli -t connection show --active | cut -d : -f 1 | grep "^\(Cal 3\|Cal 3.5\|Cal\|Cal4\|Cal 5g\|Hen Wen\|Auto Ethernet\|Wired connection 1\)$" &>/dev/null; then
-        echo "Ignoring lid close event because we're connected to: $(nmcli -t connection show --active | cut -d : -f 1)"
+    current_connections=$(nmcli -t connection show --active | cut -d : -f 1)
+    if echo "$current_connections" | grep "^\(Cal 3\|Cal 3.5\|Cal\|Cal4\|Cal 5g\|Hen Wen\|Auto Ethernet\|Wired connection 1\)$" &>/dev/null; then
+        echo "Ignoring lid close event because we're connected to: $current_connections"
     else
         echo "Lid is closed. Locking screen."
         slock
     fi
 elif grep -q open /proc/acpi/button/lid/*/state; then
     echo "Lid is open. Doing nothing."
+    exit 0
 fi
