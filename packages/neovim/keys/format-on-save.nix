b7d626ebabf759e0e96c405c1a6bebb7726975f4
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-03-10 12:06:12 -0700

    updates

--- packages/neovim/keys/format-on-save.nix
+++ packages/neovim/keys/format-on-save.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 let
   inherit (lib.nixvim) mkRaw;
@@ -36,6 +36,16 @@ in
 
   # Instead, configure none-ls to be the only formatter used by lsp-format.
   plugins.none-ls.enable = true;
+  plugins.none-ls.package = pkgs.vimPlugins.none-ls-nvim.overrideAttrs (oldAttrs: {
+    patches = (oldAttrs.patchs or [ ]) ++ [
+      # Workaround for https://github.com/nvimtools/none-ls.nvim/pull/248
+      (pkgs.fetchpatch {
+        name = "fix: adapt to LSP capability mapping change";
+        url = "https://github.com/nvimtools/none-ls.nvim/commit/0fa6ba7686c1f53a7ed8d5fd3d615f7e6be98743.patch";
+        hash = "sha256-nMA3FqVniREYP9Yg18YEG+N2sl5l+PtjTITin823hcU=";
+      })
+    ];
+  });
   # plugins.none-ls.settings.debug = true;
   plugins.none-ls.settings.enableLspFormat = true;
   plugins.none-ls.sources.formatting.nix_flake_fmt.enable = true;
