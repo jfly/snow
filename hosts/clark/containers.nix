a6df3b588ce578e3e8b2d721c154697e809c2e83
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-03-30 11:24:46 -0500

    Oops
    This was actually executing on the server :cry:

--- hosts/clark/containers.nix
+++ hosts/clark/containers.nix
@@ -44,7 +44,7 @@
             repository: docker.io/jfly/traefik
             # TODO: switch back to regular image if
             # https://github.com/traefik/traefik/pull/10130 gets merged and
-            # released (also remove overridden crd in `k8s/crds` and regenerate
+            # released (also remove overridden crd in 'k8s/crds' and regenerate
             # pulumi crds).
             tag: "2.9.10-custom-status-code-for-ip-list-middleware@sha256:c73efc904a1a03fd43f5561660039b168491243a0a7b0d8ba541367c2177591f"
             pullPolicy: IfNotPresent
