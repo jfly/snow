0bf524068e60c14ff6a4d1974ca255ebefa87b82
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-04-20 16:58:49 -0700

    Remove redundant scrape of node metrics, oops

--- hosts/fflewddur/prometheus/scrapers/up.nix
+++ hosts/fflewddur/prometheus/scrapers/up.nix
@@ -1,31 +1,9 @@
 {
-  flake,
-  lib,
   pkgs,
   ...
 }:
 {
   services.prometheus = {
-    scrapeConfigs = [
-      {
-        job_name = "up";
-        static_configs = [
-          {
-            targets = lib.pipe flake.nixosConfigurations [
-              # Get just the `config`s.
-              (lib.mapAttrsToList (_name: nixosConfiguration: nixosConfiguration.config))
-              # Filter to hosts that have the `node` exporter enabled.
-              (builtins.filter (config: config.services.prometheus.exporters.node.enable))
-              # For each host, produce a target such as "clark.ec:9000".
-              (map (
-                config: "${config.networking.fqdn}:${toString config.services.prometheus.exporters.node.port}"
-              ))
-            ];
-          }
-        ];
-      }
-    ];
-
     ruleFiles = [
       (pkgs.writeText "up.rules" (
         builtins.toJSON {
