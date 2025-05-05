448de0cd9509b5ec928917455ebf0acca70182e9
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-05-04 17:21:06 -0700

    updates and hacks

--- flake.nix
+++ flake.nix
@@ -6,43 +6,88 @@
   };
 
   inputs = {
-    # Choose not to download darwin dependencies (saves some resources on Linux, see
-    # https://github.com/ryantm/agenix#install-module-via-flakes).
-    agenix.inputs.darwin.follows = "";
-    agenix.inputs.home-manager.follows = "home-manager";
-    agenix.inputs.nixpkgs.follows = "nixpkgs";
-    agenix.url = "github:ryantm/agenix";
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs = {
+        # Choose not to download darwin dependencies (saves some resources on Linux, see
+        # https://github.com/ryantm/agenix#install-module-via-flakes).
+        darwin.follows = "";
+        home-manager.follows = "home-manager";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     flake-parts.url = "github:hercules-ci/flake-parts";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-    nix-index-database.url = "github:nix-community/nix-index-database";
+
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    nixvim.url = "github:nix-community/nixvim";
-    on-air.inputs.nixpkgs.follows = "nixpkgs";
-    on-air.url = "github:jfly/on-air";
-    openwrt-imagebuilder.url = "github:astro/nix-openwrt-imagebuilder";
-    # openwrt-imagebuilder.url = "github:jfly/nix-openwrt-imagebuilder/update-hashes";
+
+    nixvim = {
+      # I'm using a fork of nixvim with support for neovim's native `vim.lsp`
+      # API. See
+      # https://github.com/nix-community/nixvim/pull/3204#issuecomment-2849356488
+      # nixvim.url = "github:nix-community/nixvim";
+      url = "github:jfly/nixvim/use-native-vim-lsp-api";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    on-air = {
+      url = "github:jfly/on-air";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    openwrt-imagebuilder = {
+      url = "github:astro/nix-openwrt-imagebuilder";
+      # url = "github:jfly/nix-openwrt-imagebuilder/update-hashes";
+    };
+
     poetry2nix.url = "github:nix-community/poetry2nix";
+
     git-hooks-nix.url = "github:cachix/git-hooks.nix";
-    pr-tracker.inputs.nixpkgs.follows = "nixpkgs";
-    pr-tracker.url = "github:molybdenumsoftware/pr-tracker";
-    shtuff.inputs.nixpkgs.follows = "nixpkgs";
-    shtuff.url = "github:jfly/shtuff";
+
+    pr-tracker = {
+      url = "github:molybdenumsoftware/pr-tracker";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    shtuff = {
+      url = "github:jfly/shtuff";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # TODO: switch back to upstream when/if
     # https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/merge_requests/344/
     # is merged.
     # simple-nixos-mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
     simple-nixos-mailserver.url = "gitlab:jflysohigh/nixos-mailserver/dkim-path";
+
     systems.url = "github:nix-systems/x86_64-linux";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    with-alacritty.inputs.nixpkgs.follows = "nixpkgs";
-    with-alacritty.url = "github:FatBoyXPC/with-alacritty";
+
+    treefmt-nix = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/treefmt-nix";
+    };
+
+    with-alacritty = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:FatBoyXPC/with-alacritty";
+    };
   };
 
   outputs =
