5c339b0a370036be7e11af22d9e507834c001cf7
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-05-29 22:32:07 -0700

    Update to latest vaultwarden

--- iac/pulumi/app/vaultwarden.py
+++ iac/pulumi/app/vaultwarden.py
@@ -24,7 +24,7 @@ class Vaultwarden:
         snowauth.declare_app(
             name="vw",
             namespace="default",
-            image="vaultwarden/server:1.33.2",
+            image="vaultwarden/server:1.34.1",
             port=80,
             env={
                 "DOMAIN": domain,
