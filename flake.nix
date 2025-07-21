75c321ecb2645d99c76896ffed949693a2964198
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-07-21 10:36:06 -0700

    WIP: port data-mesher from `clanModules` to `clanServices`
    This does not work yet:
    
    ```
    $ nix build .#nixosConfigurations.fflewddur.config.system.build.toplevel
    ...
           error: The option `_module.args.settings' is defined multiple times while it's expected to be unique.
    
           Definition values:
           - In `/nix/store/zlkdhlalvicbs2iwx5ggabvkpikk5rbl-source/clanServices/data-mesher/default.nix, via option mappedServices.clan-core-data-mesher.roles.peer.perInstance, via option nixosModule'
           - In `/nix/store/zlkdhlalvicbs2iwx5ggabvkpikk5rbl-source/clanServices/data-mesher/default.nix, via option mappedServices.clan-core-data-mesher.roles.admin.perInstance, via option nixosModule'
           Use `lib.mkForce value` or `lib.mkDefault value` to change the priority on any of these definitions.
    ```
    
    And:
    
    ```
    $ nix build .#nixosConfigurations.pattern.config.system.build.toplevel
    ...
           error: The option `clan-core-data-mesher.instances.data-mesher.roles.peer.machines.pattern.settings.bootstrapNodes' was accessed but has no value defined. Try setting the option.
    ```

--- flake.nix
+++ flake.nix
@@ -147,17 +147,6 @@
         ];
 
         clan-core = {
-          patches = [
-            # Remove warning about deprecated data-mesher. I don't think there
-            # is a non-deprecated alternative yet, see
-            # <https://git.clan.lol/clan/clan-core/issues/3849#issuecomment-35182>
-            (fetchpatch {
-              name = "undeprecate data-mesher";
-              # Patch from <https://git.clan.lol/jfly/clan-core/compare/main...undeprecate-data-mesher>.
-              url = "https://git.clan.lol/jfly/clan-core/commit/8389e3ef36c093415d9c90acd1e4bc237ca3640a.diff";
-              hash = "sha256-3Tp3aVqBAAuovnBnpP7iiRkH/09FX2n71mx97fRZSlE=";
-            })
-          ];
           inputs.data-mesher.patches = [
             # Relax data-mesher's `NameRegex` to allow for subdomains.
             # See corresponding feature request: <https://git.clan.lol/clan/data-mesher/issues/213>.
