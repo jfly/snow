52dd7903f557d13d4e2d23dfdb830f1a183e833b
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-11-20 20:23:10 -0800

    Oh boy I should have done this years ago

--- machines/dallben/configuration.nix
+++ machines/dallben/configuration.nix
@@ -27,4 +27,12 @@
   # Give the default user `sudo` permissions. Sometimes it's nice to be able to
   # debug things with a keyboard rather than ssh-ing to the box.
   users.users.${config.services.kodi-colusita.user}.extraGroups = [ "wheel" ];
+
+  # Prevent the screen from going dark. It's a source of endless confusion when
+  # we turn on the TV to find that the screen is still dark (until we do
+  # something to "wake up" X11).
+  services.xserver.serverFlagsSection = ''
+    Option "BlankTime" "0"
+    Option "DPMS" "false"
+  '';
 }
