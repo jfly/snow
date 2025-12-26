0ed0f7ce59a04ddc11c71f75d73051ca6d9b9e50
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-12-25 22:09:20 -0800

    Fix bug with remote ssh
    Oops. Should probably figure out a way to make this user specific.

--- machines/clark/git-repos.nix
+++ machines/clark/git-repos.nix
@@ -1,7 +1,17 @@
-{ pkgs, ... }:
+{ flake, pkgs, ... }:
+let
+  inherit (flake.lib) identities;
+in
 {
   snow.backup = {
     paths = [ "/state/git" ];
+  };
+
+  # For remotes like `ssh://root@clark/state/git/<repo>.git`
+  users.users.root = {
+    openssh.authorizedKeys.keys = [
+      identities.jfly
+    ];
   };
 
   environment.systemPackages = [
