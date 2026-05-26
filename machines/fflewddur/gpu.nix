4d5ff97170f62a2cb7ab3594b13e00a25be76b0a
Jeremy Fleischman <me@jfly.fyi>
2026-05-26 16:54:59 -0700

    Old gpu!

--- machines/fflewddur/gpu.nix
+++ machines/fflewddur/gpu.nix
@@ -1,5 +1,5 @@
 # Settings from <https://wiki.nixos.org/wiki/Nvidia>
-{ lib, ... }:
+{ config, lib, ... }:
 {
   nixpkgs.config.allowUnfreePredicate =
     pkg:
@@ -12,6 +12,11 @@
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
+    # "The default NVIDIA drivers no longer support Maxwell (GTX 1xxx) or older
+    # GPUs. Pin the nvidia package to `
+    # config.boot.kernelPackages.nvidiaPackages.legacy_580` for continued
+    # support."
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
