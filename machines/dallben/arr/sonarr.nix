fc195a014db5d55adc60b97f35420da71b68ae95
Jeremy Fleischman <me@jfly.fyi>
2026-06-04 00:13:31 -0700

    Urg, yet another ipv6 issue

--- machines/dallben/arr/sonarr.nix
+++ machines/dallben/arr/sonarr.nix
@@ -1,6 +1,12 @@
-{ config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   port = config.services.sonarr.settings.server.port;
+  fflewddurIp = builtins.readFile ../../../vars/per-machine/fflewddur/zerotier/zerotier-ip/value;
 in
 {
   services.sonarr = {
@@ -31,4 +37,33 @@ in
       protocol = "tcp";
     }
   ];
+
+  # Sonarr doesn't seem to have good support for connecting to things over IPv6 [0].
+  # I haven't checked Radarr.
+  # As a workaround, set up a IPv4 -> IPv6 proxy locally for the needed
+  # services.
+  # [0]: https://github.com/Sonarr/Sonarr/issues/7534
+  #
+  # Note: this breaks IPv4 jellyfin.m outside of the network namespace. That's
+  # fine, as IPv6 still works (thanks to Happy Eyeballs).
+  networking.extraHosts = ''
+    127.0.0.1 jellyfin.m
+  '';
+  systemd.services.jellyfin-ipv4-proxy = {
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    vpnConfinement = {
+      enable = true;
+      vpnNamespace = "wg";
+    };
+
+    script = ''
+      exec ${lib.getExe pkgs.socat} TCP4-LISTEN:443,fork,reuseaddr TCP6:[${fflewddurIp}]:443
+    '';
+    serviceConfig = {
+      Type = "exec";
+      Restart = "on-failure";
+    };
+  };
 }
