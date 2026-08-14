8ef64d30076fc12995be2cd1caead7283c45c6eb
Jeremy Fleischman <me@jfly.fyi>
2026-08-14 11:47:08 -0700

    Pull cosmic patch to fix fullscreen chrome

--- machines/strongbad/cosmic.nix
+++ machines/strongbad/cosmic.nix
@@ -8,4 +8,19 @@
     enable = true;
     user = "mary";
   };
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      # Fixes an issue where chrome exits fullscreen after 1.5s.
+      cosmic-comp = prev.cosmic-comp.overrideAttrs (oldAttrs: {
+        patches = oldAttrs.patches or [ ] ++ [
+          (final.fetchpatch {
+            name = "fix(shell): take presentation feedback from fullscreen surfaces";
+            url = "https://github.com/pop-os/cosmic-comp/commit/3b8bac8335ed017a70f1ffe865c86d1ffe07c0cb.diff";
+            hash = "sha256-kBlk5vA12+UNdDAd08YycTtc1wFdT1czBeGzf9grjkg=";
+          })
+        ];
+      });
+    })
+  ];
 }
